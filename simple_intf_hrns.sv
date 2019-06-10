29a54c315c94136e5737d1fa420bacd89dd91538
Sai Sandeep L V <sandeep.lv@outlook.com>
2019-06-09 19:54:46 -0500

    Create simple_intf_hrns.sv

---
+++ simple_intf_hrns.sv
@@ -0,0 +1,17 @@
+//`include "simple_intf.sv"
+//interface simple_intf_hrns();
+  
+//  simple_intf intf(
+//   .clk    (dut.clk),
+//    .reset  (dut.reset),
+//    .in     (dut.in),
+//    .out    (dut.out),
+//  );
+  
+//  initial begin  
+//    uvm_config_db #(virtual interface simple_intf)::set(null, "*", "simple_vif", simple_intf);
+//  end
+      
+//endinterface
+  
+//bind simple simple_intf_hrns u_simple_intf_hrns();
