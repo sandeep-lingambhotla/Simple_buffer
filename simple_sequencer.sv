56bec24c0217d5bd8e01d0274a0ebcbf504f1397
Sai Sandeep L V <sandeep.lv@outlook.com>
2019-06-09 19:59:41 -0500

    Update simple_sequencer.sv

--- simple_sequencer.sv
+++ simple_sequencer.sv
@@ -1,7 +1,7 @@
 class simple_sequence extends uvm_sequence#(simple_pkt);
   `uvm_object_utils(simple_sequence)
   
-  int count;
+  //int count;
   
   function new(string name="simple_sequence");
     super.new(name);
@@ -14,10 +14,10 @@ class simple_sequence extends uvm_sequence#(simple_pkt);
       pkt = simple_pkt::type_id::create(.name("pkt"), .contxt(get_full_name()));
       start_item(pkt);
       assert(pkt.randomize());
-      if(count > 5) pkt.in = 1
+      //if(count > 5) pkt.in = 1
       //`uvm_info("pkt_created", pkt.sprint(), UVM_LOW)
       finish_item(pkt);
-      count++;
+      //count++;
     end
   endtask // body
   
